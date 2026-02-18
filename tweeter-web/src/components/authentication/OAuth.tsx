c37d1a86a47f8820681beea8d8f45bf0f7252f87
trentw0826 <trentw0826@gmail.com>
2026-02-17 17:37:18 -0700

    Tighten up OAuth logic by moving provider handling to presenter

--- tweeter-web/src/components/authentication/OAuth.tsx
+++ tweeter-web/src/components/authentication/OAuth.tsx
@@ -2,17 +2,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import { useRef } from "react";
 import { useMessageActions } from "../toaster/messageHooks";
-import { IconName } from "@fortawesome/fontawesome-svg-core";
-import { OAuthPresenter, OAuthView } from "../../presenter/OAuthPresenter";
-
-interface OAuthProps {
-  heading: string;
-}
-
-interface OAuthProvider {
-  name: string;
-  icon: IconName;
-}
+import {
+  OAuthPresenter,
+  OAuthProps,
+  OAuthView,
+} from "../../presenter/OAuthPresenter";
 
 const OAuth = ({ heading }: OAuthProps) => {
   const { displayInfoMessage } = useMessageActions();
@@ -32,30 +26,18 @@ const OAuth = ({ heading }: OAuthProps) => {
     presenterRef.current = new OAuthPresenter(listener);
   }
 
-  const providers: OAuthProvider[] = [
-    { name: "Google", icon: "google" },
-    { name: "Facebook", icon: "facebook" },
-    { name: "Twitter", icon: "twitter" },
-    { name: "LinkedIn", icon: "linkedin" },
-    { name: "GitHub", icon: "github" },
-  ];
-
-  const handleOAuthClick = (providerName: string) => {
-    presenterRef.current!.handleOAuthClick(providerName);
-  };
-
   return (
     <>
       <h1 className="h4 mb-3 fw-normal">Or</h1>
       <h1 className="h5 mb-3 fw-normal">{heading}</h1>
 
       <div className="text-center mb-3">
-        {providers.map((provider) => (
+        {presenterRef.current!.getProviders().map((provider) => (
           <button
             key={provider.name}
             type="button"
             className="btn btn-link btn-floating mx-1"
-            onClick={() => handleOAuthClick(provider.name)}
+            onClick={() => presenterRef.current!.handleOAuthClick(provider)}
           >
             <OverlayTrigger
               placement="top"
--- tweeter-web/src/presenter/OAuthPresenter.ts
+++ tweeter-web/src/presenter/OAuthPresenter.ts
@@ -1,4 +1,5 @@
 import { AuthService } from "../model.service/AuthService";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
 
 export interface OAuthView {
   displayInfoMessage: (
@@ -9,30 +10,54 @@ export interface OAuthView {
   displayErrorMessage: (message: string) => void;
 }
 
+export interface OAuthProps {
+  heading: string;
+}
+
+export interface OAuthProvider {
+  name: string;
+  icon: IconName;
+}
+
 export class OAuthPresenter {
   private _view: OAuthView;
   private _authService: AuthService;
+  private readonly _providers: OAuthProvider[] = [
+    { name: "Google", icon: "google" },
+    { name: "Facebook", icon: "facebook" },
+    { name: "Twitter", icon: "twitter" },
+    { name: "LinkedIn", icon: "linkedin" },
+    { name: "GitHub", icon: "github" },
+  ];
 
   public constructor(view: OAuthView) {
     this._view = view;
     this._authService = new AuthService();
   }
 
-  public async handleOAuthClick(providerName: string): Promise<void> {
+  public getProviders(): OAuthProvider[] {
+    return this._providers;
+  }
+
+  public async handleOAuthClick(provider: OAuthProvider): Promise<void> {
     try {
-      const isAuthenticated = await this._authService.oauthLogin(providerName);
+      const isAuthenticated = await this._authService.oauthLogin(provider.name);
 
       if (!isAuthenticated) {
         this._view.displayInfoMessage(
-          `${providerName} registration is not implemented.`,
+          `${provider.name} registration is not implemented.`,
           3000,
           "text-white bg-primary",
         );
       }
     } catch (error) {
       this._view.displayErrorMessage(
-        `Failed to authenticate with ${providerName} because of exception: ${error}`,
+        `Failed to authenticate with ${provider.name} because of exception: ${error}`,
       );
     }
+  }
+
+  public getOAuthProviders(): OAuthProvider[] {
+    return this._providers;
   }
 }
