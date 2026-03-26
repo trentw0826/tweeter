84b3fe98d094d2d1f6790c8b85db861ea3a50e5a
trentw0826 <trentw0826@gmail.com>
2026-03-25 18:36:00 -0600

    feat: Update Jest configuration and test scripts for improved integration testing

--- tweeter-server/jest.config.cjs
+++ tweeter-server/jest.config.cjs
@@ -1,6 +1,11 @@
 module.exports = {
+  verbose: true,
   testEnvironment: "node",
-  testMatch: ["<rootDir>/test/**/*.test.js"],
+  testMatch: [
+    "<rootDir>/test/unit/**/*.test.js",
+    "<rootDir>/test/integration/**/*.test.js",
+  ],
   roots: ["<rootDir>/test"],
   transform: {},
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
--- tweeter-server/test/integration/auth.integration.test.js
+++ tweeter-server/test/integration/auth.integration.test.js
@@ -1,5 +1,5 @@
 import assert from "node:assert/strict";
-import test from "node:test";
+import { test } from "@jest/globals";
 
 import { FakeData } from "tweeter-shared";
 import { handler as loginHandler } from "../../dist/auth/lambda/LoginLambda.js";
